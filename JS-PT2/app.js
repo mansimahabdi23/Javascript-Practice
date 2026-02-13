8f4467d707a55dc06714580e98152996447f9c63
mansimahabdi23 <mansimahabdi@gmail.com>
2026-02-13 13:47:02 +0530

    initial-commit

---
+++ JS-PT2/app.js
@@ -0,0 +1,8 @@
+// Literals
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+
+// console.log("total price is:", pencilPrice + eraserPrice, " Rupees. ");
+
+console.log(`Your total pay is ${pencilPrice + eraserPrice} rupees`);
