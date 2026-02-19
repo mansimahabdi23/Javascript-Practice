c87621f0225d94d33bdbd333595aba33d8aeeaf2
mansimahabdi23 <mansimahabdi@gmail.com>
2026-02-19 15:50:47 +0530

    practice-questions

--- JS-PT2/app.js
+++ JS-PT2/app.js
@@ -6,3 +6,32 @@ let eraserPrice = 5;
 // console.log("total price is:", pencilPrice + eraserPrice, " Rupees. ");
 
 console.log(`Your total pay is ${pencilPrice + eraserPrice} rupees`);
+
+// ===================== PRACTICE QUESTION =======================
+// for traffic signal
+let color = "red";
+if (color === "red") {
+  console.log("Stop! Light color is red");
+}
+
+if (color === "yellow") {
+  console.log("Slow down! Light color is yellow");
+}
+
+if (color === "green") {
+  console.log("Go! Light color is green");
+}
+
+// ==================== Else if =======================
+
+let marks = 75;
+
+if (marks >= 80) {
+  console.log("Grade A+");
+} else if (marks >= 60) {
+  console.log("Grade A");
+} else if (marks >= 33) {
+  console.log("Grade B");
+} else if (marks < 33) {
+  console.log("Grade F");
+}
