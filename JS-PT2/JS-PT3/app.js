731ca49733588f9917fd55eb6a25ff61319a4961
Mansi Mahabdi <mansimahabdi@gmail.com>
2026-03-12 15:11:38 +0530

    array-methods

--- JS-PT2/JS-PT3/app.js
+++ JS-PT2/JS-PT3/app.js
@@ -11,3 +11,17 @@ console.log(student);
 student[10] = "Smith";
 console.log(student);
 
+//push
+student.push("Emily");
+console.log(student);
+//pop
+student.pop();
+console.log(student);
+//unshift
+student.unshift("Michael");
+console.log(student);
+student.unshift("Sarah");
+console.log(student);
+//shift
+student.shift();
+console.log(student);
